bc97dd6c23d5b6736152707df786a2c253c6abd5
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-25 09:08:39 -0700

    header file

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
 #define BUFFER_SIZE 1024
 
@@ -9,14 +9,13 @@
 #include <stdio.h>
 
 /**
- * struct print_func - struct for getting the print functions
+ * struct function_struct - struct for storing the print functions
  * @argument_type: arument type
  * @function_ptr: function pointer to the function
  *
- * Description: struct that stores pointers to a
- * printer functions.
+ * Description: struct that stores pointers to a printer function
  */
-typedef struct print
+typedef struct function_struct
 {
 	char *argument_type;
 	int (*function_ptr)(va_list, char *, unsigned int);
@@ -24,13 +23,17 @@ typedef struct print
 
 
 int _printf(const char *format, ...);
+int writebuf(char *string, unsigned int nbyte);
 int (*func_output)(va_list, char *, unsigned int);
 int (*which_func(const char *str, int indx))(va_list, char *, unsigned int);
 int amount_print(const char *str, int indx);
 unsigned int manage_output(char *str, char ch, unsigned int findex);
-int writebuf(char *c, unsigned int nbyte);
+int printchr(va_list args, char *string, unsigned int findex);
+int printstr(va_list args, char *string, unsigned int findex);
+
+/*
 int printchr(va_list args, char *string, unsigned int findex);
 int printstr(va_list args, char *fout, unsigned int findex);
-
+*/
 
 #endif
